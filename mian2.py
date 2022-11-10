9855f782439b60dd5df569bc2da17e423a3f39fb
2pokek <alkuzmich6479@gmail.com>
2022-11-10 19:38:12 +0300

    2

--- mian2.py
+++ mian2.py
@@ -1,13 +1,12 @@
 import requests
 
-web ='https://jsonplaceholder.typicode.com/posts/1'
+web ='https://jsonplaceholder.typicode.com/posts/7'
 print(requests.get(web).json())
 
-response = requests.put(web,data={
+response = requests.patch(web,data={
     "id": 7,
     'userId':13,
     'title':'CRIST',
-    'body':'sewey',
 
 })
 
